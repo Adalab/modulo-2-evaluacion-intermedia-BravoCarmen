031abe0dca5be49002b4210370ab5ed09ee75397
BravoCarmen <carmenmbravo@gmail.com>
2023-09-19 09:08:33 +0200

    html and add class

---
+++ js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// 1. Elementos HTML
+const btn = document.querySelector(".js-btn");
+const select = document.querySelector(".js-select");
+const textResult = document.querySelector(".js-result");
+
+const gamerPoints = document.querySelector(".js-points_gamer");
+const computerPoints = document.querySelector(".js-points_computer");
+
+
+
+
+
+// 2. Funciones y eventos
+
